d3aac7cdf70b6f6f81636e5b42d6830b2a714c13
achinthau <achintha@duosoftware.com>
2017-08-01 11:36:34 +0530

    Update custom-environment-variables.js
    add sentinel variables

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -13,7 +13,12 @@ module.exports = {
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
 
     },
 
@@ -22,7 +27,12 @@ module.exports = {
         "ip": "SYS_REDIS_HOST",
         "port": "SYS_REDIS_PORT",
         "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
+        "password": "SYS_REDIS_PASSWORD",
+        "sentinels":{
+            "hosts": "SYS_REDIS_SENTINEL_HOSTS",
+            "port":"SYS_REDIS_SENTINEL_PORT",
+            "name":"SYS_REDIS_SENTINEL_NAME"
+        }
 
     },
 
