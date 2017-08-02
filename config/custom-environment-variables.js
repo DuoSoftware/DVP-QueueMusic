9be3e030daed0398765eb2d7ea5e502c2c4bd9c1
frodo <dasun@duosoftware.com>
2017-08-02 13:40:17 +0530

    Update custom-environment-variables.js

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -38,24 +38,6 @@ module.exports = {
 
     },
 
-    "Redis":
-    {
-        "ip": "SYS_REDIS_HOST",
-        "port": "SYS_REDIS_PORT",
-        "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
-
-    },
-
-    "Security":
-    {
-        "ip": "SYS_REDIS_HOST",
-        "port": "SYS_REDIS_PORT",
-        "user": "SYS_REDIS_USER",
-        "password": "SYS_REDIS_PASSWORD"
-
-    },
-
 
     "Host":{
         "Ip":"HOST_NAME",
