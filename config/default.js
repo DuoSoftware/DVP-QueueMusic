4a98ce66738a120ccbce30ad412962fa08565918
coder a <duoacoder@gmail.com>
2020-01-25 17:53:20 +0530

    config formatted

--- config/default.js
+++ config/default.js
@@ -1,57 +1,51 @@
 module.exports = {
- "DB": {
-    "Type":"postgres",
-    "User":"",
-    "Password":"",
-    "Port":5432,
-    "Host":"",
-    "Database":""
+  DB: {
+    Type: "postgres",
+    User: "",
+    Password: "",
+    Port: 5432,
+    Host: "",
+    Database: ""
   },
 
-
-    "Mongo":
-    {
-        "ip":"",
-        "port":"27017",
-        "dbname":"",
-        "password":"",
-        "user":"",
-        "replicaset" :""
-    },
-
-  "Redis":
-  {
-    "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "",
-    "port": 6389,
-    "user": "",
-    "password": "",
-    "sentinels":{
-      "hosts": "",
-      "port":16389,
-      "name":"redis-cluster"
-    }
-
+  Mongo: {
+    ip: "",
+    port: "27017",
+    dbname: "",
+    password: "",
+    user: "",
+    replicaset: ""
   },
 
-  "Security":
-  {
-
-    "ip" : "",
-    "port": 6389,
-    "user": "",
-    "password": "",
-    "mode":"sentinel",//instance, cluster, sentinel
-    "sentinels":{
-      "hosts": "",
-      "port":16389,
-      "name":"redis-cluster"
+  Redis: {
+    mode: "sentinel", //instance, cluster, sentinel
+    ip: "",
+    port: 6389,
+    user: "",
+    password: "",
+    sentinels: {
+      hosts: "",
+      port: 16389,
+      name: "redis-cluster"
     }
   },
 
-  "Host":{
-    "Ip":"0.0.0.0",
-    "Port":"9095",
-    "Version":"1.0.0.0"
+  Security: {
+    ip: "",
+    port: 6389,
+    user: "",
+    password: "",
+    mode: "sentinel", //instance, cluster, sentinel
+    sentinels: {
+      hosts: "",
+      port: 16389,
+      name: "redis-cluster"
+    }
+  },
+
+  Host: {
+    Ip: "0.0.0.0",
+    Port: "9095",
+    Version: "1.0.0.0"
   }
 };
